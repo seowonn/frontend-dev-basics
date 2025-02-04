8f8f99734ff10664a19a608f4034cae5b720deed
seowonn <sonseowon5@gmail.com>
2025-02-04 09:11:42 +0900

    js #1

--- javascript-practices/basics/ex01.js
+++ javascript-practices/basics/ex01.js
@@ -1,4 +1,39 @@
 console.log("============[01] 기본타입, primitive type");
+var u; /* undefined는 값이 할당되지 않은 상태를 의미한다. */
+var i = 10;
+var s = "hello world";
+var b = true;
+console.log("u: " + typeof(u));
+console.log("i: " + typeof(i));
+console.log("s: " + typeof(s));
+console.log("b: " + typeof(b));
+
 console.log("============[02.01] 객체타입: object");
+var i = new Number(10);
+var s = new String("c");
+var b = new Boolean(true);
+var o = {};
+var a = [];
+var n = null;
+console.log("i: " + typeof(i) + " " + (i instanceof(Number)));
+console.log("s: " + typeof(s) + " " + (s instanceof(String)));
+console.log("b: " + typeof(b) + " " + (b instanceof(Boolean)));
+console.log("o: " + typeof(o) + " " + (o instanceof(Object)));
+console.log("a: " + typeof(a) + " " + (a instanceof(Array)));
+console.log("n: " + typeof(n));
+
 console.log("============[02.02] 객체타입: function");
+function f1() {
+}
+var f2 = function() {
+}
+var f3 = new Function();
+console.log("f1: " + typeof(f1) + " : " + (f1 instanceof(Function)));
+console.log("f2: " + typeof(f2) + " : " + (f2 instanceof(Function)));
+console.log("f3: " + typeof(f3) + " : " + (f3 instanceof(Function)));
+
 console.log("============[06] 기본타입과 유사객체");
+var b1 = true;
+var b2 = new Boolean(false);
+console.log(b1.valueOf());
+console.log(b2.valueOf());
--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -1,3 +1,20 @@
 console.log("============[02.02] 표현식(Expression)");
+i = 4
+2 + 2
+parseInt('4')
+Math.pow(2, 2)
+4
+
 console.log("============[05] 세미콜론(Semicolon)");
+a = 10; b = 20;
+
 console.log("============[06] 개행(Line Break)");
+k
+=
+10
+console.log(k)
+
+// error: undefined error
+console.lo; /* console의 lo라는 메서드는 찾아보았으나 발견되지 않아 undefined인 상태 */
+console.log("hi"); /* 정상 출력됨 */
+// g('hello world') /* g로 선언된 객체가 없어 여기서 에러가 발생 */
