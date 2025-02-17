31b5f78680759dea956506cfa950d0df3937d7de
seowonn <sonseowon5@gmail.com>
2025-02-18 06:33:04 +0900

    async callback 비동기 함수 실습습

--- javascript-practices/async/ex01.js
+++ javascript-practices/async/ex01.js
@@ -1,4 +1,32 @@
 /*
     callback 지원 비동기 함수 사용
 */
+const {fn01} = require('./fns');
+
+// test: success
+fn01('param~~', function(error, result) {
+    if(error) {
+        console.error(error);
+        return;
+    }
+    console.log(`result: ${result}`);
+});
+
+// test: fail
+// fn01(null, function(error, result) {
+//     if(error) {
+//         console.error(error);
+//         return;
+//     }
+//     console.log(`result: ${result}`);
+// })
+
+console.log('wait....');
+
+// 무거운 연산 (10초 소요): Call Stack에 있는 내용이 다 끝나야 콜백함수가 실행된다.
+const start = Date.now();
+while (Date.now() - start < 10000) {} // 10초 동안 Call Stack 점유
+
+console.log("무거운 연산 종료");
+
 
--- javascript-practices/async/fns.js
+++ javascript-practices/async/fns.js
@@ -3,3 +3,41 @@
     1. fn01: callback 지원 비동기함수
     2. fn02: promise 지원 비동기함수
 */
+
+exports.fn01 = function(param, callback) {
+    /*
+     비동기 코드... ex) file io, network, SQL to DB, setTimer
+    */
+    console.log("fn01: some code run....");
+
+    setTimeout(function() {
+        if(param != null) {
+            // result: success
+            callback(null, {result: 'success'});
+            console.log('비동기 함수: callback 함수 실행 완료')
+        } else {
+            // fail: error
+            callback(new Error('fail'), null);
+        }
+    }, 3000);
+}
+
+exports.fn02 = function(param) {
+    /*
+     비동기 코드... ex) file io, network, SQL to DB, setTimer
+    */
+    return new Promise(() => {
+        console.log("fn02: some code run....");
+
+        setTimeout(function() {
+            if(param != null) {
+                // result: success
+                resolve({result: 'success'});
+            } else {
+                // fail: error
+                reject(new Error('fail'));
+            }
+        }, 3000);
+    });
+
+}
